513717561e15054b25ce725072b0c76b6f582542
Péter Pandula <pandulapeter@gmail.com>
2022-10-29 14:34:17 +0300

    Add support for night mode.

--- presentation/src/main/java/com/chignonMignon/wallpapers/presentation/feature/collections/CollectionsColorTransitionManager.kt
+++ presentation/src/main/java/com/chignonMignon/wallpapers/presentation/feature/collections/CollectionsColorTransitionManager.kt
@@ -2,6 +2,7 @@ package com.chignonMignon.wallpapers.presentation.feature.collections
 
 import android.animation.ValueAnimator
 import android.content.Context
+import android.graphics.Color
 import androidx.core.graphics.ColorUtils
 import com.chignonMignon.wallpapers.presentation.R
 import com.chignonMignon.wallpapers.presentation.feature.Navigator
@@ -9,46 +10,34 @@ import com.chignonMignon.wallpapers.presentation.utilities.extensions.color
 import com.chignonMignon.wallpapers.presentation.utilities.extensions.colorResource
 
 internal class CollectionsColorTransitionManager(
-    private val context: Context,
+    context: Context,
     private val updateColors: (primaryColor: Int, secondaryColor: Int, onSecondaryColor: Int) -> Unit
 ) {
-    private var primaryColor: Int? = null
-    private var secondaryColor: Int? = null
-    private var onSecondaryColor: Int? = null
+    private val defaultPrimaryColor = context.color(R.color.primary)
+    private var primaryColor = defaultPrimaryColor
+    private val defaultSecondaryColor = context.colorResource(android.R.attr.windowBackground)
+    private var secondaryColor = defaultSecondaryColor
+    private val defaultOnSecondaryColor = context.colorResource(android.R.attr.textColorPrimary)
+    private var onSecondaryColor = defaultOnSecondaryColor
 
     fun updateColors(collection: Navigator.Collection?) {
-        if (primaryColor == null || secondaryColor == null || onSecondaryColor == null) {
-            val windowBackgroundColor = context.colorResource(android.R.attr.windowBackground)
-            if (primaryColor == null) {
-                primaryColor = windowBackgroundColor
+        val currentPrimaryColor = primaryColor
+        val newPrimaryColor = collection?.colorPalette?.primary ?: defaultPrimaryColor
+        val currentSecondaryColor = secondaryColor
+        val newSecondaryColor = collection?.colorPalette?.secondary ?: defaultSecondaryColor
+        val currentOnSecondaryColor = onSecondaryColor
+        val newOnSecondaryColor = collection?.colorPalette?.onSecondary ?: defaultOnSecondaryColor
+        ValueAnimator.ofFloat(0f, 1f).apply {
+            addUpdateListener {
+                updateColors(
+                    ColorUtils.blendARGB(currentPrimaryColor, newPrimaryColor, it.animatedFraction),
+                    ColorUtils.blendARGB(currentSecondaryColor, newSecondaryColor, it.animatedFraction),
+                    ColorUtils.blendARGB(currentOnSecondaryColor, newOnSecondaryColor, it.animatedFraction)
+                )
             }
-            if (secondaryColor == null) {
-                secondaryColor = windowBackgroundColor
-            }
-            if (onSecondaryColor == null) {
-                onSecondaryColor = context.color(R.color.on_primary)
-            }
-        }
-        val newPrimaryColor = collection?.colorPalette?.primary ?: context.color(R.color.primary)
-        val newSecondaryColor = collection?.colorPalette?.secondary ?: context.colorResource(android.R.attr.windowBackground)
-        val newOnSecondaryColor = collection?.colorPalette?.onSecondary ?: context.color(R.color.on_primary)
-        primaryColor?.let { currentPrimaryColor ->
-            secondaryColor?.let { currentSecondaryColor ->
-                onSecondaryColor?.let { currentOnSecondaryColor ->
-                    ValueAnimator.ofFloat(0f, 1f).apply {
-                        addUpdateListener {
-                            updateColors(
-                                ColorUtils.blendARGB(currentPrimaryColor, newPrimaryColor, it.animatedFraction),
-                                ColorUtils.blendARGB(currentSecondaryColor, newSecondaryColor, it.animatedFraction),
-                                ColorUtils.blendARGB(currentOnSecondaryColor, newOnSecondaryColor, it.animatedFraction)
-                            )
-                        }
-                    }.start()
-                    primaryColor = newPrimaryColor
-                    secondaryColor = newSecondaryColor
-                    onSecondaryColor = newOnSecondaryColor
-                }
-            }
-        }
+        }.start()
+        primaryColor = newPrimaryColor
+        secondaryColor = newSecondaryColor
+        onSecondaryColor = newOnSecondaryColor
     }
 }
