2140327eb0e83c3b09931bd9a20ccb2a11a8e76d
Péter Pandula <pandulapeter@gmail.com>
2022-11-01 10:17:56 +0200

    Improve the appearance of the wallpaper details screen.

--- presentation/src/main/java/com/chignonMignon/wallpapers/presentation/utilities/extensions/ViewExtensions.kt
+++ presentation/src/main/java/com/chignonMignon/wallpapers/presentation/utilities/extensions/ViewExtensions.kt
@@ -5,7 +5,6 @@ import android.graphics.drawable.Animatable
 import android.graphics.drawable.Drawable
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowInsets
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
