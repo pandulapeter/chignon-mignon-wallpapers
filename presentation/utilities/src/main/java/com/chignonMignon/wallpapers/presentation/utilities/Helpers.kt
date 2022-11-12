f070e52ce07d1def0e95852241d17f24bc18299f
Péter Pandula <pandulapeter@gmail.com>
2022-11-12 13:28:48 +0200

    Improve screen transitions.

--- presentation/utilities/src/main/java/com/chignonMignon/wallpapers/presentation/utilities/Helpers.kt
+++ presentation/utilities/src/main/java/com/chignonMignon/wallpapers/presentation/utilities/Helpers.kt
@@ -2,6 +2,8 @@ package com.chignonMignon.wallpapers.presentation.utilities
 
 import android.graphics.Color
 import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.AnticipateOvershootInterpolator
+import android.view.animation.BounceInterpolator
 import com.google.android.material.transition.MaterialContainerTransform
 import com.google.android.material.transition.MaterialSharedAxis
 import kotlinx.coroutines.channels.BufferOverflow
@@ -16,9 +18,10 @@ fun sharedElementTransition() = MaterialContainerTransform().apply {
     scrimColor = Color.TRANSPARENT
     fadeMode = MaterialContainerTransform.FADE_MODE_OUT
     interpolator = AccelerateDecelerateInterpolator()
-    duration = 300
+    duration = 400
 }
 
 fun enterTransition(forward: Boolean) = MaterialSharedAxis(MaterialSharedAxis.Y, forward).apply {
-    duration = 600
+    duration = 400
+    interpolator = AccelerateDecelerateInterpolator()
 }
