6ec77732ac42aab44ab80bd3aa4d18d92707c5e7
Péter Pandula <pandulapeter@gmail.com>
2022-11-15 13:20:52 +0200

    Improve about screen padding and navigation bar color.

--- presentation/shared/src/main/java/com/chignonMignon/wallpapers/presentation/shared/customViews/AboutView.kt
+++ presentation/shared/src/main/java/com/chignonMignon/wallpapers/presentation/shared/customViews/AboutView.kt
@@ -5,7 +5,9 @@ import android.util.AttributeSet
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.widget.LinearLayout
+import com.chignonMignon.wallpapers.presentation.shared.R
 import com.chignonMignon.wallpapers.presentation.shared.databinding.ViewAboutBinding
+import com.chignonMignon.wallpapers.presentation.utilities.extensions.dimension
 
 class AboutView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
@@ -15,5 +17,9 @@ class AboutView @JvmOverloads constructor(
         orientation = VERTICAL
         gravity = Gravity.CENTER_HORIZONTAL
         ViewAboutBinding.inflate(LayoutInflater.from(context), this)
+        context.dimension(R.dimen.double_content_padding).let {
+            // TODO: Bottom padding should take into account the bottom inset
+            setPadding(paddingLeft, it, paddingRight, it)
+        }
     }
 }
