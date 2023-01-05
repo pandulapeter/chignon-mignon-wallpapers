7ae2f8b3d05061e66117e0665d8c7d81483fd3f9
Péter Pandula <pandulapeter@gmail.com>
2023-01-05 13:55:00 +0200

    Create an animated vector drawable for the loading indicator.

--- presentation/shared/src/main/java/com/chignonMignon/wallpapers/presentation/shared/customViews/LogoSwiperRefreshLayout.kt
+++ presentation/shared/src/main/java/com/chignonMignon/wallpapers/presentation/shared/customViews/LogoSwiperRefreshLayout.kt
@@ -1,9 +1,12 @@
 package com.chignonMignon.wallpapers.presentation.shared.customViews
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.widget.ImageView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat.AnimationCallback
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
 import com.chignonMignon.wallpapers.presentation.shared.R
 
 class LogoSwiperRefreshLayout @JvmOverloads constructor(
@@ -11,7 +14,14 @@ class LogoSwiperRefreshLayout @JvmOverloads constructor(
     attrs: AttributeSet? = null,
 ) : SwipeRefreshLayout(context, attrs) {
 
+    private val animatedVectorDrawable = AnimatedVectorDrawableCompat.create(context, R.drawable.avd_logo_animation)?.apply {
+        registerAnimationCallback(object : AnimationCallback() {
+            override fun onAnimationEnd(drawable: Drawable?) = start()
+        })
+        start()
+    }
+
     init {
-        (getChildAt(0) as ImageView).setImageResource(R.drawable.img_loading_indicator_interior)
+        animatedVectorDrawable?.let { (getChildAt(0) as? ImageView)?.setImageDrawable(it) }
     }
 }
